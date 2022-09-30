da293b75e737f538105321a7da76789d1b00e7dd
Alfredo Sena <33937487+avsena@users.noreply.github.com>
2022-09-30 03:05:06 -0300

    Criei o relógio e o data completa no JavaScript

--- função.js
+++ função.js
@@ -1,3 +1,20 @@
 {
-  alert("Você clicou no botão");
+    //alert("Olá");
+
+    //função-data (DIA/MÊS/ANO)
+    var horaData = new Date();
+    var dia = horaData.getDate();
+    var mes = horaData.getMonth();
+    var ano = horaData.getFullYear();
+
+    meses = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
+
+    //função-hora (HH:MM)
+    var horas = horaData.getHours();
+    var minutos = horaData.getMinutes();
+
+    document.write(horas + ":" + minutos + "<br/>");//preciso aprender a conectar no id do HTML
+
+    document.getElementById("data").innerHTML =//estou querendo conectar a data na class do html. A data some*/
+    (document.write(dia + " de " + meses[mes] + " de " + ano));
 }
